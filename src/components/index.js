5cac8da22a9002cc8505119b2d012767266dde90
Andrew Melnychuk <a.melnychuk93@gmail.com>
2020-10-02 01:05:06 +0300

    add login and register pages

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export { default as Accordion } from "./accordion";
 export { default as EmailForm } from "./email-form";
 export { default as Header } from "./header";
 export { default as Feature } from "./feature";
+export { default as Form } from "./form";
